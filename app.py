ee47f131ef84b19c4a97624eb96639784d3aa379
tranvananhanhanh <139228186+tranvananhanhanh@users.noreply.github.com>
2024-01-13 22:36:02 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 # Import necessary modules
 from flask import Flask, render_template, request, jsonify
+import os
 from dotenv import load_dotenv
 from openai import AzureOpenAI
 from quickstart import client
+from flask_cors import cross_origin
+from flask import Flask, render_template, request
 from voice import text_to_speech
-import requests  
 
 
+
+
+# Load environment variables
 load_dotenv()
 
+
+# Create Flask app
 app = Flask(__name__)
+
 
 # Print the OpenAI API key for debugging purposes
 print(client.api_key)
@@ -17,9 +25,9 @@ print(client.api_key)
 # Define the route for the homepage
 @app.route('/')
 def index():
-    return render_template('main.html')
+    return render_template('i2.html')
 
-
+# Define the route for handling user input and providing responses
 @app.route('/get_response', methods=['POST'])
 def get_response():
     # Get user input from the form
@@ -29,41 +37,24 @@ def get_response():
     response = client.chat.completions.create(
         model="GPT35TURBO16K",
         messages=[
-            {"role": "system", "content": "Bạn là giáo viên toán"},
+            {"role": "system", "content": "bạn là giáo viên toán"},
             {"role": "user", "content": user_input},
         ]
+    
     )
+    print(response.choices[0].message.content)
+    # Check the console output for the structure of the response
+   
 
-    # Access the content based on the new structure
-    response_content = response.choices[0].message.content
-    print(response_content)
     try:
         # Access the content based on the new structure
-        response_content = response.choices[0].message.content
-        print(response_content)
-        return jsonify({'response': response_content})
+        response_content =response.choices[0].message.content
+        return jsonify({'response': str(response_content)}) 
     except TypeError as e:
         # Handle the error and provide a meaningful response
         print(f"Error: {e}")
         return jsonify({'response': 'Error in processing the response'})
-    
-
-@app.route('/text_to_speech', methods=['POST'])
-def text_to_speech_route():
-    if request.method == 'POST':
-        text = request.form['speech']
-        gender = 'male'
-        rate = 200
-        volum = 0.1
-        text_to_speech(text, gender, rate, volum)
-        return jsonify({'status': 'success'})
-    else:
-        return jsonify({'status': 'error', 'message': 'Invalid request method'})
-
-
-
-
 
 # Run the app if the script is executed
 if __name__ == '__main__':
-    app.run(debug=True, port=1700)
+    app.run(debug=True,port=1700)
